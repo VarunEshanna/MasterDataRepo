c711a8065135d369471fc168e27a6541918ee692
Varun <eshanna@adobe.com>
2017-10-01 16:26:50 +0530

    removing files

--- src/main/java/com/adobe/assistance/response/CreateCase.java
+++ src/main/java/com/adobe/assistance/response/CreateCase.java
@@ -21,8 +21,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import com.adobe.assistance.beans.ResponseData;
